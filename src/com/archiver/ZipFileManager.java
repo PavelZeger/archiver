e59178857adc0f77d6805cd34c73f429272515c0
PavelZeger <gabberoid@gmail.com>
2021-01-22 23:16:26 +0200

    Added createZip() in ZipFileManager

--- src/com/archiver/ZipFileManager.java
+++ src/com/archiver/ZipFileManager.java
@@ -1,6 +1,10 @@
 package com.archiver;
 
+import java.io.InputStream;
+import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipOutputStream;
 
 /**
  * @author Pavel Zeger
@@ -13,9 +17,27 @@ public class ZipFileManager {
 
     public ZipFileManager(Path zipFile) {
         this.zipFile = zipFile;
+
     }
 
     public void createZip(Path source) throws Exception {
+        try (ZipOutputStream zipOutputStream = new ZipOutputStream(Files.newOutputStream(zipFile))) {
+            ZipEntry zipEntry = new ZipEntry(source.getFileName().toString());
+            zipOutputStream.putNextEntry(zipEntry);
+
+            byte[] buffer = new byte[1024];
+            int length;
+            try (InputStream inputStream = Files.newInputStream(source)) {
+                while ((length = inputStream.read(buffer)) > 0) {
+                    zipOutputStream.write(buffer, 0, length);
+
+                }
+
+                zipOutputStream.closeEntry();
+
+            }
+
+        }
 
     }
 
