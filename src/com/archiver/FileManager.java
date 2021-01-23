f35bb88473660ce06ff380b4d033246184b0b560
PavelZeger <gabberoid@gmail.com>
2021-01-23 18:29:53 +0200

    Added FileManager

---
+++ src/com/archiver/FileManager.java
@@ -0,0 +1,44 @@
+package com.archiver;
+
+import java.io.IOException;
+import java.nio.file.DirectoryStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author Pavel Zeger
+ * @implNote Archiver
+ * @since 23/01/2021
+ */
+public class FileManager {
+
+    private Path rootPath;
+    private List<Path> fileList = new ArrayList<>();
+
+    public FileManager(Path rootPath) throws IOException {
+        this.rootPath = rootPath;
+        collectFileList(rootPath);
+    }
+
+    public List<Path> getFileList() {
+        return fileList;
+    }
+
+    private void collectFileList(Path path) throws IOException {
+        if (Files.isRegularFile(path)) {
+            getFileList().add(rootPath.relativize(path));
+        }
+        if (Files.isDirectory(path)) {
+            try (DirectoryStream<Path> directoryStream = Files.newDirectoryStream(path)) {
+                for (Path entry : directoryStream) {
+                    collectFileList(entry);
+                }
+            }
+
+        }
+
+    }
+
+}
